e9dc3046808be42bea0ed74936e54fcdb38f358e
Valerie Hayot <valeriehayot@gmail.com>
2017-02-12 15:41:43 -0500

    added min/max calculation option

--- python/VoxelMapper.py
+++ python/VoxelMapper.py
@@ -26,35 +26,31 @@ def mapper(bin_size, data_max, data_min):
             #if same voxel value is encountered again, increment the count of the voxel value by one
             for value in data:
                 try:
-                    voxel_value = value.item()
+                    voxel_value = float(value.item())
                 except:
-                    print('Error: Voxel value could not be cast to a python scalar')
+                    print('Error: Voxel value could not be cast to a float')
                     sys.exit(1)
-                
                 #dictionary key will be in the form of (min, max) tuple. All values will be in min/max range, excluding the max, which will be the min value of the proceeding bin
-                if isinstance(value, numbers.Number):
+                try:
                     
-                    if data_min is not None and data_max is not None and (voxel_value < data_min or voxel_value >= data_max):
+                    if (voxel_value < data_min or voxel_value > data_max):
                         continue
 
                     remainder = voxel_value % bin_size
 
                     #create bins. Bins intervals are of format [min, max[ (max is excluded)                    
-                    if data_min is None or data_max is None: 
-                        min_bin_value = voxel_value - remainder
-                        max_bin_value = voxel_value + bin_size - remainder
-                    else:
-                        min_bin_value = voxel_value - remainder if voxel_value - remainder >= data_min and voxel_value - remainder <= data_max else data_min 
-                        max_bin_value = voxel_value + bin_size -remainder if voxel_value + bin_size - remainder <= data_max and voxel_value + bin_size - remainder >= data_min  else data_max
+                    min_bin_value = voxel_value - remainder if voxel_value - remainder >= data_min and voxel_value - remainder <= data_max else data_min 
 
-
-                    key = round(min_bin_value, 3), round(max_bin_value, 3)
+                    key = round(min_bin_value, 3)
 
                     if key in histogram:
                         histogram[key] += 1
                     else:
                         histogram[key] = 1
-
+                except:
+                    print('Error: error occured while generating histogram')
+                    sys.exit(1)
+            
             for key, value in histogram.items():
                 print("%s\t%d" % (key,value))
 
@@ -62,62 +58,44 @@ def main():
     
     parser = argparse.ArgumentParser(description="Generate histogram of nifti-1 image")
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument("-nb", "--num_bins", help="The number of bins in your histogram (integer value), max value (float), min value (float)*. ", action='append', nargs='+')
-    group.add_argument("-br", "--bin_range", help="The interval range of the bins (float), max value (float)*, min value(float)*", action='append', nargs='+')
+    group.add_argument("-nb", "--num_bins", help="The number of bins in your histogram (integer value). ", type=int)
+    group.add_argument("-br", "--bin_range", help="The interval range of the bins (float)", type=float)
+    parser.add_argument("--min_val", help="The minimum voxel value of the image", type=float, required=True)
+    parser.add_argument("--max_val", help="The maximum voxel value of the image", type=float, required=True)
 
-    number_bins = None
-    data_min = None
-    data_max = None
-    bin_size = 1
 
-    try:
-        args= parser.parse_args()
-    except:
-        parser.print_usage()
-        sys.exit(1)
+    args=parser.parse_args()
 
-    if args.num_bins:
+
+
+    number_bins = args.num_bins
+    bin_range = args.bin_range if args.bin_range else 1
+    
+    data_min = args.min_val
+    data_max = args.max_val
+
+    
+    if number_bins:
         try:
-            number_bins = int(args.num_bins[0][0])
-            data_max = float(args.num_bins[0][1])
-            
-            if len(args.num_bins[0]) > 2:
-                data_min = float(args.num_bins[0][2])
-            else:
-                data_min = 0
+            #added a '+1' such that the data_max would be included in result without creating an extra bin
+            bin_range = round((data_max - data_min + 1) / number_bins, 3 )
         except:
-            print("Error: --num_bins argument must be in the format int float float*")
+            print('Error: --num_bins cannot be 0')
             sys.exit(1)
 
-        try:
-            bin_size = round((data_max - data_min) / number_bins, 5)
-        except:
-            print('Error: --num_bins cannot be 0.')
-            sys.exit(1)        
 
-    elif args.bin_range:
-        try:
-            bin_size = float(args.bin_range[0][0])
-            if len(args.bin_range[0]) == 2:
-                data_max = float(args.bin_range[0][1])
-                data_min = 0
-            elif len(args.bin_range[0]) > 2:
-                data_max = float(args.bin_range[0][1])
-                data_min = float(args.bin_range[0][2])
-        except:
-            print("Error: --bin_range argument must be in the format float float* float*")
-            sys.exit(1)
+
     
     try:
-        if bin_size == 0:
+        if bin_range == 0:
             raise ValueError('bin_size cannot be 0')
         if data_min >= data_max:
             raise ValueError('min value cannot be greater or equal to max')
     except ValueError as error:
         print('Error:' +  error.args[0])
         sys.exit(1)
-
-    mapper(bin_size, data_max, data_min)
+    
+    mapper(bin_range, data_max, data_min)
                     
 if __name__ == "__main__": main()
 
