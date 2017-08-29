d237c79006bb852c105b67e562cabf526dfdec68
Tristan Glatard <tristan.glatard@concordia.ca>
2017-08-29 16:44:19 -0700

    template map job

--- bids/bids-pipeline.py
+++ bids/bids-pipeline.py
@@ -8,6 +8,10 @@ def create_RDD(bids_dataset_root,sc):
     layout = BIDSLayout(bids_dataset_root)
     return sc.parallelize(layout.get_subjects())
 
+def list_files_by_participant(bids_dataset, participant_name):
+    array = find bids_dataset/sub-participant_name -type f
+    return array
+
 def main():
     # Spark initialization
     conf = SparkConf().setAppName("log_analyzer").setMaster("local")
@@ -16,8 +20,10 @@ def main():
     parser=argparse.ArgumentParser()
     parser.add_argument("bids_dataset", help="BIDS dataset to be processed")
     args=parser.parse_args()
+    bids_dataset = args.bids_dataset
     
-    rdd = create_RDD(args.bids_dataset,sc)
+    rdd = create_RDD(bids_dataset,sc)
+    rdd.map(lambda x: list_files_by_participant(bids_dataset,x))
     
     print(rdd.collect())
     
