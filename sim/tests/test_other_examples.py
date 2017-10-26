a6e13db5956bba95543d3e29bf80b7d57501190e
Tushar Gupta <tushargupta98@gmail.com>
2017-10-25 20:19:24 -0400

    Issue #64 Minor fixes on the gen_histo example Issue #49 Write tests for histogram computation

--- sim/tests/test_other_examples.py
+++ sim/tests/test_other_examples.py
@@ -7,7 +7,7 @@ import sys
 class TestOther_examples(TestCase):
     ## UTILITY METHODS
     def get_oexamples_dir(self):
-        return os.path.join(os.path.dirname(__file__), "../../other-examples")
+        return os.path.join(os.path.dirname(__file__), "../../other-examples/")
 
     def get_data_dir(self):
         return os.path.join(os.path.dirname(__file__), "demo/ds001/sub-01/anat")
@@ -15,14 +15,15 @@ class TestOther_examples(TestCase):
     ## TESTS
     def test_create_histo(self):
         actual_result=""
-        command = [os.path.join(self.get_oexamples_dir()
-                                , "gen_histo.py")
-            , "-b", "5"
-            , os.path.join("./", self.get_data_dir())]
+        command=[os.path.join(self.get_oexamples_dir()
+                                       ,"gen_histo.py"),
+                                       "-b","5",
+                                       self.get_data_dir()]
         try:
-            actual_result = subprocess.call(command)
+            actual_result = subprocess.check_output(command)
         except:
             e = sys.exc_info()[0]
             print("<p>Error: %s</p>" % e )
-        expected_result = "[(0.0, 6412336), (819.0, 26374), (1638.0, 163), (2457.0, 26), (3276.0, 13)]"
+        expected_result = "[(0.0, 6412336), (819.0, 26374), (1638.0, 163), (2457.0, 26), (3276.0, 13)]\n"
         self.assertTrue(actual_result == expected_result)
+
